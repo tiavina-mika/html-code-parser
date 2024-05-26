33f98f1121101fc5751cf97e35f3dc69e5fc975e
tiavina-mika <tiavinamika@gmail.com>
2024-05-26 20:20:38 +0300

    Update type.d.ts

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,37 +1,16 @@
-import { ReactNode } from "react";
+export type HtmlCodeParserOptions = {
+  /*
+  *
+  * Specify the programming language of the code snippet that
+  * will be displayed in the `ReadOnlyTextCodeEditor` component. By default, the `language` prop is set to
+  * `'jsx'`, but it can be overridden when using the component by passing a different language value.
+  *
+  * for more language options, visit: https://github.com/wooorm/lowlight?tab=readme-ov-file#data
+  */
+  language?: string;
+  codeContainerClassName?: string;
+};
 
-export interface ITextEditorOption<T = string> {
-  label: string;
-  value: T,
-  icon?: string | ReactNode;
-  hide?: boolean;
-}
-
-export interface ITextEditorCollaborationUser {
-  name: string;
-  color: string;
-}
-
-export enum EditorToolbarEnum {
-  heading = 'heading',
-  bold = 'bold',
-  italic = 'italic',
-  strike = 'strike',
-  link = 'link',
-  underline = 'underline',
-  image = 'image',
-  code = 'code',
-  orderedList = 'orderedList',
-  bulletList = 'bulletList',
-  align = 'align',
-  codeBlock = 'codeBlock',
-  blockquote = 'blockquote',
-  table = 'table',
-  history = 'history',
-  youtube = 'youtube',
-  color = 'color',
-  mention = 'mention',
-  ai = 'ai'
-}
-
-export type IEditorToolbar = `${EditorToolbarEnum}`;
+export type HtmlCodeParserProps = {
+  children: string;
+} & HtmlCodeParserOptions;
