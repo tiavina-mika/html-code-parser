74b69816b30151bbfbb55110a1eed0d31ec7e392
tiavina-mika <tiavinamika@gmail.com>
2024-05-27 11:32:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite'
+import { UserConfig, defineConfig } from 'vite'
+import { resolve } from 'path';
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  plugins: [dts({ rollupTypes: true }), react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'html-code-parser',
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ["es"]
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  // resolve: { alias: { src: resolve('src/') } },
   server: {
     open: true,
   },
-})
+} satisfies UserConfig)
